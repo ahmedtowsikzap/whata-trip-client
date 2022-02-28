7ab06d16be81120d131e07d741afc96ee61ee18d
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-03-01 02:17:08 +0600

    added responsive navigation bar &  video section

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../../Shared/Navbar";
+import TopNotch from "../TopNotch/TopNotch";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar />
+      <TopNotch />
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -1,7 +1,157 @@
-import React from "react";
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+import { motion } from "framer-motion";
+import logo from "../../Assets/images/baggage.png";
 
 const Navbar = () => {
-  return <div></div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <nav className="bg-white-300 shadow-lg shadow-red-100">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <motion.div
+                className="flex-shrink-0"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1.3, duration: 1.7 }}
+                whileTap={{ scale: 0.6 }}
+              >
+                <img className="h-12 w-12 mr-1" src={logo} alt="logo" />
+              </motion.div>
+              <motion.div
+                className="hidden md:block flex-shrink-0 ml-3"
+                initial={{ x: "-100vw" }}
+                animate={{ x: 0 }}
+                transition={{ delay: 0.5 }}
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.6 }}
+              >
+                <h2 className="italic text-xl font-medium ">
+                  <span className="underline decoration-fuchsia-500">What</span>
+                  <span className="italic ml-1 text-2xl text-pink-800 font-medium">
+                    A
+                  </span>
+                  <span className="text-2xl font-bold text-red-600">Trip!</span>
+                </h2>
+              </motion.div>
+              <motion.div
+                className="hidden md:block"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 1.7, duration: 1.8 }}
+              >
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <a
+                    href="https://google.com"
+                    className=" hover:bg-pink-300 text-gray-800 px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Dashboard
+                  </a>
+
+                  <a
+                    href="https"
+                    className="text-gray-800 hover:bg-amber-500 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    About
+                  </a>
+
+                  <a
+                    href="httos"
+                    className="text-gray-800 hover:bg-purple-400 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Admin
+                  </a>
+                </div>
+              </motion.div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-white-900 inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-white hover:bg-red-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <Transition
+          show={isOpen}
+          enter="transition ease-in-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-100 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-100"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className="hover:bg-gray-700 text-orange-400 sblock px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Dashboard
+                </a>
+
+                <a
+                  href="#"
+                  className="text-orange-400 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Team
+                </a>
+
+                <a
+                  href="#"
+                  className="text-orange-400 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Projects
+                </a>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
